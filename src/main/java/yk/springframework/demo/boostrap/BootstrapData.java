905cb49720e25ca6c712c6fd386bd85ea8c4c810
Yi Kuang <y24kuang@uwaterloo.ca>
2023-05-09 17:33:07 -0400

    2.9 Initializing Data with Spring

---
+++ src/main/java/yk/springframework/demo/boostrap/BootstrapData.java
@@ -0,0 +1,43 @@
+package yk.springframework.demo.boostrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import yk.springframework.demo.domain.Author;
+import yk.springframework.demo.domain.Book;
+import yk.springframework.demo.repositories.AuthorRepository;
+import yk.springframework.demo.repositories.BookRepository;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "3939459459");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
+    }
+}
