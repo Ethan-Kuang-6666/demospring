2901e471086164dbbd4638270fb6f6d7186b58e1
Yi Kuang <y24kuang@uwaterloo.ca>
2023-05-10 11:31:46 -0400

    2.13 Controller setup

---
+++ src/main/java/yk/springframework/demo/controllers/BookController.java
@@ -0,0 +1,25 @@
+package yk.springframework.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import yk.springframework.demo.repositories.BookRepository;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "Books";
+    }
+
+}
